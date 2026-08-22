3fcd213d3cecdacf8c7b6bada030d7355c8496f2
bv9522 <bvella9522@gmail.com>
2026-08-21 22:55:27 -0400

    feat: define Phase 2B manifest file discovery

--- core/Wintainium.Core/Private/Manifest/Find-WintainiumManifestFile.ps1
+++ core/Wintainium.Core/Private/Manifest/Find-WintainiumManifestFile.ps1
@@ -3,17 +3,28 @@ function Find-WintainiumManifestFile {
     param(
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$ManifestRoot
+        [string]$ManifestRoot,
+
+        [switch]$Recurse
     )
 
     $resolvedRoot = (Resolve-Path -LiteralPath $ManifestRoot -ErrorAction Stop).Path
+    $getChildItemParameters = @{
+        LiteralPath = $resolvedRoot
+        Filter = '*.wintainium.json'
+        File = $true
+        ErrorAction = 'Stop'
+    }
+
+    if ($Recurse) {
+        $getChildItemParameters.Recurse = $true
+    }
 
     $manifests = @(
-        Get-ChildItem -LiteralPath $resolvedRoot -Filter '*.json' -File -ErrorAction Stop |
+        Get-ChildItem @getChildItemParameters |
             Sort-Object -Property FullName |
             ForEach-Object { $_.FullName }
     )
 
     return ,$manifests
 }
-
