97589c9fa5260d83715e5793c318b6a56e0510b0
bv9522 <bvella9522@gmail.com>
2026-08-31 23:16:34 -0400

    refactor: inject HTTP client for download testability

--- core/Wintainium.Core/Private/Download/Invoke-WintainiumDownload.ps1
+++ core/Wintainium.Core/Private/Download/Invoke-WintainiumDownload.ps1
@@ -5,42 +5,42 @@ function Invoke-WintainiumDownload {
         [psobject]$DownloadRequest,
 
         [Parameter(Mandatory)]
-        [string]$DownloadRoot
+        [string]$DownloadRoot,
+
+        [Parameter()]
+        [System.Net.Http.HttpClient]$HttpClient
     )
 
     $target = Resolve-WintainiumDownloadTarget -DownloadRequest $DownloadRequest -DownloadRoot $DownloadRoot
-
     $root = $target.DownloadRoot
     [System.IO.Directory]::CreateDirectory($root) | Out-Null
-
     $temporaryPath = Join-Path $root ([System.IO.Path]::GetRandomFileName())
+    $ownsClient = $null -eq $HttpClient
+
+    if ($ownsClient) {
+        $HttpClient = [System.Net.Http.HttpClient]::new()
+    }
 
     try {
-        $client = [System.Net.Http.HttpClient]::new()
+        $response = $HttpClient.GetAsync($target.Uri, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead).GetAwaiter().GetResult()
         try {
-            $response = $client.GetAsync($target.Uri, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead).GetAwaiter().GetResult()
+            $response.EnsureSuccessStatusCode() | Out-Null
+            $stream = $response.Content.ReadAsStreamAsync().GetAwaiter().GetResult()
             try {
-                $response.EnsureSuccessStatusCode() | Out-Null
-                $stream = $response.Content.ReadAsStreamAsync().GetAwaiter().GetResult()
+                $fileStream = [System.IO.File]::Open($temporaryPath, [System.IO.FileMode]::CreateNew, [System.IO.FileAccess]::Write, [System.IO.FileShare]::None)
                 try {
-                    $fileStream = [System.IO.File]::Open($temporaryPath, [System.IO.FileMode]::CreateNew, [System.IO.FileAccess]::Write, [System.IO.FileShare]::None)
-                    try {
-                        $stream.CopyToAsync($fileStream).GetAwaiter().GetResult()
-                    }
-                    finally {
-                        $fileStream.Dispose()
-                    }
+                    $stream.CopyToAsync($fileStream).GetAwaiter().GetResult()
                 }
                 finally {
-                    $stream.Dispose()
+                    $fileStream.Dispose()
                 }
             }
             finally {
-                $response.Dispose()
+                $stream.Dispose()
             }
         }
         finally {
-            $client.Dispose()
+            $response.Dispose()
         }
 
         [System.IO.File]::Move($temporaryPath, $target.DestinationPath)
@@ -58,5 +58,10 @@ function Invoke-WintainiumDownload {
             Remove-Item -LiteralPath $temporaryPath -Force -ErrorAction SilentlyContinue
         }
         throw
+    }
+    finally {
+        if ($ownsClient) {
+            $HttpClient.Dispose()
+        }
     }
 }
