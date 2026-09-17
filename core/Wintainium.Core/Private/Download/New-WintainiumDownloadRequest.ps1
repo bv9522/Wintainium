9ec92ce75f6de8122031751c8c31061d8db4d51c
bv9522 <bvella9522@gmail.com>
2026-09-16 20:08:32 -0400

    Preserve orchestration correlation in download requests

--- core/Wintainium.Core/Private/Download/New-WintainiumDownloadRequest.ps1
+++ core/Wintainium.Core/Private/Download/New-WintainiumDownloadRequest.ps1
@@ -1,34 +1,28 @@
 function New-WintainiumDownloadRequest {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory)]
-        [psobject]$UpdateDecision
+        [Parameter(Mandatory)] [psobject]$UpdateDecision,
+        [string]$OperationId
     )
 
-    if ($null -eq $UpdateDecision) {
-        throw [System.ArgumentNullException]::new('UpdateDecision')
-    }
-
+    if ($null -eq $UpdateDecision) { throw [System.ArgumentNullException]::new('UpdateDecision') }
     foreach ($requiredProperty in @('Status', 'IsUpdateAvailable', 'SelectedRelease', 'SelectedArtifact')) {
-        if (-not $UpdateDecision.PSObject.Properties[$requiredProperty]) {
-            throw [System.ArgumentException]::new("UpdateDecision is missing required property '$requiredProperty'.")
-        }
+        if (-not $UpdateDecision.PSObject.Properties[$requiredProperty]) { throw [System.ArgumentException]::new("UpdateDecision is missing required property '$requiredProperty'.") }
     }
+    if ([string]$UpdateDecision.Status -ne 'UpdateAvailable' -or -not [bool]$UpdateDecision.IsUpdateAvailable) { throw [System.ArgumentException]::new('Only an UpdateAvailable decision can be converted into a download request.') }
+    if ($null -eq $UpdateDecision.SelectedRelease) { throw [System.ArgumentException]::new('An UpdateAvailable decision must contain a selected release.') }
+    if ($null -eq $UpdateDecision.SelectedArtifact) { throw [System.ArgumentException]::new('An UpdateAvailable decision must contain a selected artifact.') }
 
-    if ([string]$UpdateDecision.Status -ne 'UpdateAvailable' -or -not [bool]$UpdateDecision.IsUpdateAvailable) {
-        throw [System.ArgumentException]::new('Only an UpdateAvailable decision can be converted into a download request.')
-    }
-
-    if ($null -eq $UpdateDecision.SelectedRelease) {
-        throw [System.ArgumentException]::new('An UpdateAvailable decision must contain a selected release.')
-    }
-
-    if ($null -eq $UpdateDecision.SelectedArtifact) {
-        throw [System.ArgumentException]::new('An UpdateAvailable decision must contain a selected artifact.')
+    $resolvedOperationId = if ([string]::IsNullOrWhiteSpace($OperationId)) {
+        [guid]::NewGuid().ToString()
+    } else {
+        $parsed = [guid]::Empty
+        if (-not [guid]::TryParse($OperationId, [ref]$parsed)) { throw [System.ArgumentException]::new('OperationId must be a valid GUID.') }
+        $parsed.ToString()
     }
 
     [pscustomobject][ordered]@{
-        OperationId = [guid]::NewGuid().Guid
+        OperationId = $resolvedOperationId
         UpdateDecision = $UpdateDecision
         SelectedRelease = $UpdateDecision.SelectedRelease
         SelectedArtifact = $UpdateDecision.SelectedArtifact
