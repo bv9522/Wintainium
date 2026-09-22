1d549c847335c6ac46dda1584a93b64392769d47
bv9522 <bvella9522@gmail.com>
2026-09-22 16:52:10 -0400

    Phase 12.5B: use Pester 3 assertion syntax

--- tests/Unit/OfficialDownloadPageSourceResolution.Tests.ps1
+++ tests/Unit/OfficialDownloadPageSourceResolution.Tests.ps1
@@ -13,45 +13,45 @@ Describe 'Wintainium official download page source resolution' {
             param($uri)
             Invoke-WintainiumProviderSourceResolution -Request ([pscustomobject]@{OperationId='page-test-1';SourceUri=$uri})
         } 'https://www.7-zip.org/download.html'
-        $result.IsSuccessful|Should -Be $true
-        $result.Status|Should -Be 'Resolved'
-        $result.OperationId|Should -Be 'page-test-1'
-        $result.Source.Name|Should -Be '7-Zip'
-        $result.Source.CanonicalUri|Should -Be 'https://www.7-zip.org/download.html'
-        $result.Source.ProviderId|Should -Be 'Wintainium.provider.official-download-page'
-        $result.Source.ProviderSettings.pageUri|Should -Be 'https://www.7-zip.org/download.html'
+        $result.IsSuccessful | Should Be $true
+        $result.Status | Should Be 'Resolved'
+        $result.OperationId | Should Be 'page-test-1'
+        $result.Source.Name | Should Be '7-Zip'
+        $result.Source.CanonicalUri | Should Be 'https://www.7-zip.org/download.html'
+        $result.Source.ProviderId | Should Be 'Wintainium.provider.official-download-page'
+        $result.Source.ProviderSettings.pageUri | Should Be 'https://www.7-zip.org/download.html'
     }
     It 'accepts supported metadata in different attribute order' {
         Mock Invoke-WebRequest {[pscustomobject]@{Content='<html><head><meta content="VideoLAN" property="og:site_name"><meta content="VLC media player" name="application-name"><title>VLC download</title></head></html>';Headers=@{'Content-Type'='text/html'}}} -ModuleName Wintainium.provider.official-download-page
         $result=& (Get-Module Wintainium.provider.official-download-page) {Invoke-WintainiumProviderSourceResolution -Request ([pscustomobject]@{OperationId='page-test-2';SourceUri='https://www.videolan.org/vlc/'})}
-        $result.IsSuccessful|Should -Be $true
-        $result.Source.Name|Should -Be 'VLC media player'
-        $result.Source.Publisher|Should -Be 'VideoLAN'
+        $result.IsSuccessful | Should Be $true
+        $result.Source.Name | Should Be 'VLC media player'
+        $result.Source.Publisher | Should Be 'VideoLAN'
     }
     It 'does not return download artifacts during source resolution' {
         Mock Invoke-WebRequest {$script:response} -ModuleName Wintainium.provider.official-download-page
         $result=& (Get-Module Wintainium.provider.official-download-page) {Invoke-WintainiumProviderSourceResolution -Request ([pscustomobject]@{OperationId='page-test-3';SourceUri='https://www.7-zip.org/download.html'})}
-        $result.Source.PSObject.Properties.Name|Should -Not -Contain 'Artifacts'
-        $result.Source.PSObject.Properties.Name|Should -Not -Contain 'DownloadUri'
+        $result.Source.PSObject.Properties.Name | Should Not Contain 'Artifacts'
+        $result.Source.PSObject.Properties.Name | Should Not Contain 'DownloadUri'
     }
     It 'rejects non-HTML content' {
         Mock Invoke-WebRequest {[pscustomobject]@{Content='MZ';Headers=@{'Content-Type'='application/octet-stream'}}} -ModuleName Wintainium.provider.official-download-page
         $result=& (Get-Module Wintainium.provider.official-download-page) {Invoke-WintainiumProviderSourceResolution -Request ([pscustomobject]@{OperationId='page-test-4';SourceUri='https://example.com/download'})}
-        $result.IsSuccessful|Should -Be $false
-        $result.Status|Should -Be 'SourceUnsupported'
-        $result.Errors.Code|Should -Contain 'OfficialDownloadPageContentTypeUnsupported'
+        $result.IsSuccessful | Should Be $false
+        $result.Status | Should Be 'SourceUnsupported'
+        $result.Errors.Code | Should Contain 'OfficialDownloadPageContentTypeUnsupported'
     }
     It 'fails deterministically when identity metadata is absent' {
         Mock Invoke-WebRequest {[pscustomobject]@{Content='<html><body><p>Downloads</p></body></html>';Headers=@{'Content-Type'='text/html'}}} -ModuleName Wintainium.provider.official-download-page
         $result=& (Get-Module Wintainium.provider.official-download-page) {Invoke-WintainiumProviderSourceResolution -Request ([pscustomobject]@{OperationId='page-test-5';SourceUri='https://example.com/download'})}
-        $result.IsSuccessful|Should -Be $false
-        $result.Status|Should -Be 'SourceResponseInvalid'
-        $result.Errors.Code|Should -Contain 'OfficialDownloadPageIdentityMissing'
+        $result.IsSuccessful | Should Be $false
+        $result.Status | Should Be 'SourceResponseInvalid'
+        $result.Errors.Code | Should Contain 'OfficialDownloadPageIdentityMissing'
     }
     It 'does not request an unsupported URI scheme' {
         Mock Invoke-WebRequest {throw 'Network access must not occur.'} -ModuleName Wintainium.provider.official-download-page
         $result=& (Get-Module Wintainium.provider.official-download-page) {Invoke-WintainiumProviderSourceResolution -Request ([pscustomobject]@{OperationId='page-test-6';SourceUri='ftp://example.com/download'})}
-        $result.IsSuccessful|Should -Be $false
-        $result.Status|Should -Be 'SourceUnsupported'
+        $result.IsSuccessful | Should Be $false
+        $result.Status | Should Be 'SourceUnsupported'
     }
 }
