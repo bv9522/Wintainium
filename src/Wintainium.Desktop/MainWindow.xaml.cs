632835fbf13aa5b8e26335d5f6d80598de5109cf
bv9522 <bvella9522@gmail.com>
2026-09-24 19:05:41 -0400

    Pass authoritative installed-state service to application details

--- src/Wintainium.Desktop/MainWindow.xaml.cs
+++ src/Wintainium.Desktop/MainWindow.xaml.cs
@@ -107,7 +107,7 @@ public sealed partial class MainWindow : Window
                 return;
             }
 
-            var window = new ApplicationDetailsWindow(application, _services.ApplicationRelease, _services.ApplicationUpdate);
+            var window = new ApplicationDetailsWindow(application, _services.ApplicationRelease, _services.ApplicationUpdate, _services.InstalledApplicationState);
             _applicationDetailsWindows[application.ApplicationId] = window;
             window.Closed += (_, _) => _applicationDetailsWindows.Remove(application.ApplicationId);
             App.TrackWindow(window);
