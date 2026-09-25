202f7e500472b3ae09e88696c2d65f36e938a817
bv9522 <bvella9522@gmail.com>
2026-09-24 23:35:52 -0400

    Phase 13D: fix production Windows uninstall registry path

--- plugins/Wintainium.reconciliation.windows-installed-application/Wintainium.reconciliation.windows-installed-application.psm1
+++ plugins/Wintainium.reconciliation.windows-installed-application/Wintainium.reconciliation.windows-installed-application.psm1
@@ -46,7 +46,7 @@ function Get-WintainiumWindowsInstalledApplicationCandidates {
         $reader = {
             param($location)
 
-            $subKeyPath = 'SoftwareMicrosoftWindowsCurrentVersionUninstall'
+            $subKeyPath = 'Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall'
             $hive = switch ([string]$location.scope) {
                 'machine' { [Microsoft.Win32.RegistryHive]::LocalMachine; break }
                 'user' { [Microsoft.Win32.RegistryHive]::CurrentUser; break }
